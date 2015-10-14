5a74fd397aeddec34b45983e5322d7859278647d
Ben Schreiber <bschreib@ualberta.ca>
2015-10-14 17:17:45 -0600

    Update Tweet.java

--- app/src/main/java/ca/ualberta/cs/lonelytwitter/Tweet.java
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/Tweet.java
@@ -1,44 +1,9 @@
 package ca.ualberta.cs.lonelytwitter;
 
-<<<<<<< HEAD
-import android.text.BoringLayout;
-
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.IllegalFormatCodePointException;
-
-/**
- * Created by bschreib on 9/16/15.
- */
-public abstract class Tweet implements Tweetable{
-
-    private String text;
-    private Date date;
-    private ArrayList<Mood>  moodList = new ArrayList <Mood>();
-
-    public Tweet(String tweet, Date date) {
-        this.setText(tweet);
-        this.setDate(date);
-    }
-
-    public Tweet(String tweet) {
-        this.setText(tweet);
-        this.setDate(new Date());
-    }
-
-    public Date getDate() {
-        return date;
-    }
-
-    public void setDate(Date date) {
-        this.date = date;
-=======
 import java.io.IOException;
 import java.util.Date;
 
-/**
- * Created by joshua2 on 9/16/15.
- */
+
 public abstract class Tweet extends Object implements Tweetable {
     private String text;
     protected Date date;
@@ -51,14 +16,12 @@ public abstract class Tweet extends Object implements Tweetable {
     public Tweet(String tweet) throws TweetTooLongException {
         this.setText(tweet);
         this.date = new Date();
->>>>>>> f15IOlab
     }
 
     public String getText() {
         return text;
     }
 
-<<<<<<< HEAD
     public void setText(String text) {
         if (text.length() <= 140) {
             this.text = text;
@@ -69,7 +32,6 @@ public abstract class Tweet extends Object implements Tweetable {
 
     public abstract Boolean isImportant();
 
-=======
     public void setText(String text) throws TweetTooLongException {
         if (text.length() <= 140) {
             this.text = text;
@@ -82,16 +44,5 @@ public abstract class Tweet extends Object implements Tweetable {
     public String toString() {
         return date.toString() + " || " + this.text;
     }
-
-    public Date getDate() {
-        return date;
-    }
-
-    public void setDate(Date date) {
-        this.date = date;
-    }
-
-    public abstract Boolean isImportant();
->>>>>>> f15IOlab
 
 }
