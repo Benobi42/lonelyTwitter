d0e1e84bcf7c20a53ddcc03d953a2a479528c2d5
Ben Schreiber <bschreib@ualberta.ca>
2015-10-14 17:19:00 -0600

    Update Tweetable.java

--- app/src/main/java/ca/ualberta/cs/lonelytwitter/Tweetable.java
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/Tweetable.java
@@ -2,13 +2,6 @@ package ca.ualberta.cs.lonelytwitter;
 
 import java.util.Date;
 
-/**
-<<<<<<< HEAD
- * Created by bschreib on 9/16/15.
-=======
- * Created by joshua2 on 9/16/15.
->>>>>>> f15IOlab
- */
 public interface Tweetable {
     public String getText();
     public Date getDate();
